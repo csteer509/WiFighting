4247ed5e0fc413160610be372dc61dae17a17fdd
Cory Steer <coryb24@gmail.com>
2020-11-12 08:41:57 -0500

    Added a sniffing class to sniff mac addresses.

---
+++ macsniffer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from scapy.all import *
+
+class MACSniffer:
+
+	def __init__(self):
+		self.macs = []
+
+	def handleProbes(self, pkt):
+		if pkt.haslayer(Dot11):
+			if pkt.type == 0 and pkt.subtype == 4:
+				self.macs.append(pkt.addr2)
+
+	#Count is the total number of packets to sniff before stopping. Not the number of probe requests to sniff before stopping
+	def sniffProbes(self, iface, count = 60):
+		sniff(iface=iface, prn=self.handleProbes, count=count)
+		
+	def getMACs(self):
+		return self.macs
